21cd26c22066d3f9004e50adace5e5db42ebb460
Tauã Ferreira <86664818+tauz-hub@users.noreply.github.com>
2022-06-18 17:46:22 -0300

    Update media.java

--- media.java
+++ media.java
@@ -23,3 +23,18 @@ public class media {
 
   }
 }
+/*
+Média ou mediana
+A média de três números inteiros A, B e C é (A + B + C)/3. A mediana de três números inteiros é o número que ficaria no meio se os três números fossem ordenados em ordem não-decrescente.
+
+Sua tarefa é escrever um programa que, dados dois números inteiros distintos A e B, calcule o menor inteiro possível C tal que a média e a mediana de A, B e C sejam iguais.
+
+Entrada
+A entrada é composta de uma única linha contendo dois números inteiros A e B.
+
+Saída
+Seu programa deve produzir uma única linha, contendo um único número, o menor inteiro possível C tal que a média e a mediana de A, B e C são iguais.
+
+Restrições
+1 ≤ A ≤ B ≤ 109
+*/
